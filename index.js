993d45b1a3306152e06620f6e076ae66eb517320
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-07-14 02:51:04 -0400

    FBI System

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import db from './database/config.js'
 
+const secretPassword = 'secreto'
+
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -28,11 +30,11 @@ app.get('/', (req, res) => {
 app.post('/SignIn', async (req, res) => {
     try {
         let { email, password } = req.body;
-        //log (email, password)
+        //Verificar que lleguen los datos
         if (!email || !password) {
-            return res.status(400).json({message: 'Debe proporcionar todos los datos para la autenticación.'})
+            return res.status(400).json({ message: 'Debe proporcionar todos los datos para la autenticación.' })
         }
-        //Verificar usuario
+        //Verificar si el usuario existe
         let consulta = {
             text: 'SELECT id, email FROM usuarios WHERE email = $1 AND password = $2',
             values: [email, password]
@@ -46,13 +48,70 @@ app.post('/SignIn', async (req, res) => {
                 message: "Credenciales inválidas."
             })
         };
-        res.status(200).json({data: respuesta.rows, message: 'Login correcto.'});
+
+        //Generación token jwt
+        const token = jwt.sign(usuario, secretPassword)
+        // redirect
+        res.status(200).json({ data: respuesta.rows, message: 'Login correcto.', token});
     } catch (error) {
         log(error.message)
-        res.status(500).json({message: 'Error interno del servidor.'})
+        res.status(500).json({ message: 'Error interno del servidor.' })
+    }
+});
+
+
+const verificarToken = (req, res, next) => {
+    try {
+        let { authorization } = req.headers;
+        if (!authorization){
+            return res.status(401).json({
+                message: 'Debe proporcionar un token.'
+            })
+        };
+        const token = authorization.split(' ')[1];
+        const decoded = jwt.verify(token, secretPassword);
+        log('DECODED', decoded)
+        req.usuario = decoded
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            message: 'Debe proporcionar un token válido.'
+        })
     }
 
-});
+}
+
+// Ruta protegida -> Permite obtener la informacion de un usuario por su id
+app.get('/api/v1/usuarios/:id', verificarToken, async (req, res) => {
+    try {
+        let { id } =req.params;
+        if ( id != req.usuario.id){
+            return res.status(403).json({
+                message: 'Usted no tiene permiso para visualizar información de otro usuario.'
+            })
+        } 
+        const consulta = {
+            text: 'SELECT id, email, password FROM usuarios WHERE id = $1',
+            values: [id]
+        }
+        const { rows } = await db.query(consulta)
+        const usuario = rows[0]
+        if (!usuario) {
+            return res.status(400).json({
+                message: 'Por favor verifique los datos enviados.'
+            })
+        };
+        res.json({
+            usuario
+        });
+
+    } catch (error) {
+        log(error.message)
+        res.status(500).json({
+            message: 'Error al intentar obtener la data del usuario.'
+        })
+    }
+}); 
 
 
 app.listen(3000, () => {
