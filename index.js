49f49ba8cdc7bfc620511ae827040009b26cf142
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-07-13 23:34:47 -0400

    Reemplazo archivo agentes.js por base de datos pg. Creacion ruta sign in

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import jwt from 'jsonwebtoken';
+import fs from 'fs';
+import db from './database/config.js'
 
 import * as path from 'path';
 import { fileURLToPath } from 'url';
@@ -21,6 +23,28 @@ app.use(express.static('public'));
 //RUTA PÁGINA PRINCIPAL
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/index.html'));
+});
+
+app.post('/SignIn', async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        log (email, password)
+        if (!email || !password) {
+            return res.status(400).json({message: 'Debe proporcionar todos los datos para la autenticación.'})
+        }
+        let consulta = {
+            text: 'SELECT email, password FROM usuarios WHERE email = $1',
+            values: [email]
+        };
+        let respuesta = await db.query(consulta)
+        //log('RESPUESTA: ', respuesta.rows)
+        //log('Email:', respuesta.rows[0].email, 'Password:', respuesta.rows[0].password);
+        res.status(200).json(respuesta.rows);
+    } catch (error) {
+        log(error.message)
+        res.status(500).json({message: 'Error interno del servidor.'})
+    }
+
 });
 
 
